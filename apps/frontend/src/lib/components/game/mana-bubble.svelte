bb2c7084bc445159532faf15da6ad2fab4d311f2
Tim Feid <tim@timfeid.com>
2024-10-05 16:19:07 -0400

    chore: let's go

---
+++ apps/frontend/src/lib/components/game/mana-bubble.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { DeckSelector, ManaType } from '@gangsta/rusty';
+
+	export let color: ManaType;
+</script>
+
+<div
+	class="w-3 h-3 rounded-full"
+	class:bg-green-400={color.toLowerCase() === 'green'}
+	class:bg-blue-400={color.toLowerCase() === 'blue'}
+	class:bg-gray-950={color.toLowerCase() === 'black'}
+></div>
