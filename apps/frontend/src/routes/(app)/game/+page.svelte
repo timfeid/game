ada83649134b1103da45bcbcb010de382c8f5459
Tim Feid <tim@timfeid.com>
2024-10-17 16:50:19 -0400

    chore: a good spot

---
+++ apps/frontend/src/routes/(app)/game/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Game from '$lib/components/game/Game.svelte';
+
+	$: code = $page.url.searchParams.get('code');
+</script>
+
+{#if code}
+	<Game {code} />
+{/if}
